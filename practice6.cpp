38c27d39c2eb66780c0e1b07961e8b4941174988
Manpreet Mann <manpreetmann4788@gmail.com>
2024-06-05 13:57:53 +0530

    practiced questions

--- practice6.cpp
+++ practice6.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
+// factorial
 int main()
 {
     int num;
-    cin >> num;
-    int sum = 0;
+    cin>>num;
+    int multiple = 1;
     for (int i = 1; i <= num; i++)
     {
-        sum = sum + i;
+        multiple = multiple * i;
     }
-    cout << sum;
+    cout << multiple;
 
     return 0;
 }
